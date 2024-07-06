15b958e4dfe89f217efc96da763f8749a2ce1ae0
Joseph Lomas <ed19j5l@leeds.ac.uk>
2024-07-06 12:20:49 +0100

    Actual fix.

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 from dotenv import load_dotenv
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True 
 
